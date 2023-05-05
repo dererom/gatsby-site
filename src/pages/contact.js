32aba2ef08053e9ec788290e00c4d1d944cf8754
Roman <romandereshev1@gmail.com>
2023-05-05 14:03:25 +0200

    repair contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+// Step 2: Define your component
+const ContactPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <form method="post" action="https://formspree.io/f/xpzebyvq">
+                <span>
+                    Name
+                    <input type="text" name="name" id="name"/>
+                </span>
+                <label>
+                    Email
+                    <input type="email" name="_replyto"/>
+                </label>
+                <label>
+                    Subject
+                    <input type="text" name="subject" id="subject"/>
+                </label>
+                <label>
+                    Message
+                    <textarea name="message" id="message" rows="5"/>
+                </label>
+                <label>
+                    <input type="text" name="_gotcha" style="display:none"/>
+                </label>
+                <button type="submit">Send</button>
+                <input type="reset" value="Clear"/>
+            </form>
+        </Layout>
+        )
+}
+
+export const Head = () => <Seo title="Contact"/>
+
+// Step 3: Export your component
+export default ContactPage
